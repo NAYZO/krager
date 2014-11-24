fa1c9f4ea9c50bb59b34c7212847855bfc6d643b
nayzo <alakhefifi@gmail.com>
2014-11-24 14:37:23 +0100

    Update Content

--- src/Nzo/KragerBundle/Controller/KragerController.php
+++ src/Nzo/KragerBundle/Controller/KragerController.php
@@ -6,9 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class KragerController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('NzoKragerBundle:Krager:index.html.twig',
-            array('name' => $name));
+        return $this->render('NzoKragerBundle:Krager:index.html.twig');
     }
 }
